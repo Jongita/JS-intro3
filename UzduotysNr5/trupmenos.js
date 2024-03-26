912089377e5e8db1253c3735e03327cb1644e7cd
Jongita <jurgitajonaityte@gmail.com>
2024-03-26 09:33:37 +0200

    pataisymai

--- UzduotysNr5/trupmenos.js
+++ UzduotysNr5/trupmenos.js
@@ -1,78 +1,107 @@
-// 1. Sukurkime klasę Trupmena, ją turėtų sudaryti trys atributai: sveikojiDalis, skaitiklis, daliklis
+// Užduotis „Trupmenos“
 
+// 1. Sukurkime klasę Trupmena, ją turėtų sudaryti trys atributai: sveikojiDalis, skaitiklis, daliklis
+// 2. Sukurkime klasei setter‘ius ir getter‘ius
+// 3. Klasei Trupmena sukurkime metodą toString kuris gražintų trupmeną tokiu formatu: sveikojiDalis skaitiklis / daliklis(pvz.: 1 2 / 5)
+// 4. Ištestuokime programinį kodą sukurdami objektą, suteikime kintamiesiems reikšmes ir išveskime rezultatą.
+// 5. Sukurkime metodą: pridetiInt(sveikasisSkaicius) kuris pridėtų sveikąjį skaičių prie trupmenos, ištestuokime pakoreguotą kodą.
+// 6. Sukurkite dar vieną papildomą metodą: prideti(sveikasisSkaicius, skaitiklis, vardiklis).Nepamirškite jog pridedant skaičių reikia subendravardiklinti).
+// 7. Sukurkime metodą pridetiTrupmena(x), šis metodas turės prie esamos trupmenos pridėti paduotą trupmeną.Nepamirškite jog taip pat po šio veiksmo reikės suprastinti trupmeną.
+// 8. Sukurkime metodą toDouble() kuris grąžintų esamą trupmenos reikšmę realiuoju skaičiumi(per kablelį).
+
+// Papildomai padarykite:
+// 1. Sukurkime metodą prastinti(), jis turėtų suprastinti trupmeną.Metodą padarykime matomą tik pačiam objektui(jis turi būti nepasiekiamas iš išorės).
+// 2. Padarykime jog po kiekvieno seterio ir po pridėjimo metodų įvykdymo būtų iškviečiamas prastinimo metodas.
 
 class Trupmena {
-    #s = 0;
-    #skait = 0;
-    #dal = 0;
-    constructor(s, skait, dal) {
-        this.#s = s;
-        this.#skait = skait;
-        this.#dal = dal;
+    #sveikojiDalis = 0;
+    #skaitiklis = 0;
+    #daliklis = 0;
+
+    constructor(sveikojiDalis, skaitiklis, daliklis) {
+        this.#sveikojiDalis = sveikojiDalis;
+        this.#skaitiklis = skaitiklis;
+        this.#daliklis = daliklis;
     }
-    // 2. Sukurkime klasei setter‘ius ir getter‘ius
-    set s(s) {
-        this.#s = s;
+
+    set sveikojiDalis(sveikojiDalis) {
+        this.#sveikojiDalis = sveikojiDalis;
+        this.#prastinti;
     }
-    get s() {
-        return this.#s;
+
+    get sveikojiDalis() {
+        return this.#sveikojiDalis
     }
-    set skait(skait) {
-        this.#skait = skait;
+
+    set skaitiklis(skaitiklis) {
+        this.#skaitiklis = skaitiklis;
+        this.#prastinti;
     }
-    get skait() {
-        return this.#skait;
+
+    get skaitiklis() {
+        return this.#skaitiklis
     }
-    set dal(dal) {
-        this.#dal = dal;
+
+    set daliklis(daliklis) {
+        this.#daliklis = daliklis;
+        this.#prastinti;
     }
-    get dal() {
-        return this.#dal;
+
+    get daliklis() {
+        return this.#daliklis
     }
-    // 3. Klasei Trupmena sukurkime metodą toString kuris gražintų trupmeną tokiu formatu: sveikojiDalis skaitiklis / daliklis(pvz.: 1 2 / 5)
+
     toString() {
-        return `${this.#s} ${this.#skait} / ${this.#dal}`;
+        return `${this.sveikojiDalis === 0 ? '' : this.sveikojiDalis} ${this.skaitiklis} / ${this.daliklis}`;
     }
-    // 5. Sukurkime metodą: pridetiInt(sveikasisSkaicius) kuris pridėtų sveikąjį skaičių prie trupmenos, ištestuokime pakoreguotą kodą.
 
-    pridetiInt(sSk) {
-        this.s += sSk;
-        return `${this.s} ${this.skait} / ${this.dal}`;
+    pridetiInt(sveikasisSkaicius) {
+        return this.#sveikojiDalis += sveikasisSkaicius;
     }
-    // 6. Sukurkite dar vieną papildomą metodą: prideti(sveikasisSkaicius, skaitiklis, vardiklis).Nepamirškite jog pridedant skaičių reikia subendravardiklinti).
-    papildomaTrupmena(s2, skait2, dal2) {
-        this.s += s2;
-        if (dal2 === this.dal) {
-            return `${this.s} ${this.skait + skait2} / ${this.dal}`;
-        } else {
-            return `${this.s} ${(this.skait * dal2) + (skait2 * this.dal)} / ${this.dal * dal2}`;
+
+    #prastinti() {
+        for (let i = this.skaitiklis; i > 0; i--) {
+            if (this.daliklis % i === 0 && this.skaitiklis % i === 0) {
+                this.#daliklis = this.daliklis / i;
+                this.#skaitiklis = this.skaitiklis / i;
+            }
+            break;
         }
     }
 
-    // 7. Sukurkime metodą pridetiTrupmena(x), šis metodas turės prie esamos trupmenos pridėti paduotą trupmeną.Nepamirškite jog taip pat po šio veiksmo reikės suprastinti trupmeną.
+    prideti(sveikasisSkaicius, skaitiklis, vardiklis) {
+        const senosTrupmenosSkaitiklis = this.sveikojiDalis * this.daliklis + this.skaitiklis;
+        const paduotosTrupmenosSkaitiklis = sveikasisSkaicius * vardiklis + skaitiklis;
+        const bendrasSkaitiklis = senosTrupmenosSkaitiklis * vardiklis + paduotosTrupmenosSkaitiklis * this.daliklis;
+        const bendrasVardiklis = this.daliklis * vardiklis;
+        if (bendrasSkaitiklis > bendrasVardiklis && bendrasSkaitiklis % bendrasVardiklis !== 0) {
+            this.sveikojiDalis = Math.floor(bendrasSkaitiklis / bendrasVardiklis);
+            this.daliklis = bendrasVardiklis;
+            this.skaitiklis = bendrasSkaitiklis % bendrasVardiklis;
+        } else if (bendrasSkaitiklis % bendrasVardiklis === 0) {
+            this.sveikojiDalis = this.sveikojiDalis + sveikasisSkaicius + 1;
+            this.daliklis = 0;
+            this.skaitiklis = 0;
+        } else {
+            this.sveikojiDalis = 0;
+            this.daliklis = bendrasVardiklis;
+            this.skaitiklis = bendrasSkaitiklis;
+        }
+        this.#prastinti();
+        return `${this.sveikojiDalis === 0 ? '' : this.sveikojiDalis} ${this.skaitiklis === 0 ? '' : this.skaitiklis} ${this.skaitiklis === 0 && this.daliklis === 0 ? '' : '/'} ${this.daliklis === 0 ? '' : this.daliklis}`;
+    }
+
     pridetiTrupmena(x) {
-        if (x.dal === this.dal) {
-            return `${this.s + x.s} ${this.skait + x.skait} / ${this.dal}`;
-        } else {
-            return `${this.s + x.s} ${(this.skait * x.dal) + (x.skait * this.dal)} / ${this.dal * x.dal}`;
-        }
-    }
-    // 8. Sukurkime metodą toDouble() kuris grąžintų esamą trupmenos reikšmę realiuoju skaičiumi(per kablelį).
-    toDouble() {
-        return `${this.s + eval(this.skait / this.dal)}`;
+        return this.prideti(x.sveikojiDalis, x.skaitiklis, x.daliklis);
     }
 
+    toDouble() {
+        return (this.#sveikojiDalis * this.#daliklis + this.#skaitiklis) / this.#daliklis;
+    }
 }
 
+const trupm1 = new Trupmena(1, 2, 3);
+const trupm2 = new Trupmena(0, 27, 32);
 
 
-
-// 4.Ištestuokime programinį kodą sukurdami objektą, suteikime kintamiesiems reikšmes ir išveskime rezultatą.
-const a = new Trupmena(1, 2, 5);
-const b = new Trupmena(1, 3, 5);
-console.log(a.toString());
-console.log(a.pridetiInt(2));
-console.log(a.papildomaTrupmena(2, 3, 4));
-console.log(a.pridetiTrupmena(b));
-console.log(a.toDouble());
-
+console.log(trupm1.pridetiTrupmena(trupm2));
